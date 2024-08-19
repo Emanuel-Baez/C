163c0a19aa448c62df326720121d808fff2238d3
Emanuel <emanuelbenitezbaez@gmail.com>
2024-08-19 13:12:57 -0300

    Estructura iterativa for

---
+++ EstructuraFor/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 15
+int main()
+{
+    //ESTRUCTURA ITERATIVA: FOR
+    int i;
+    int resultado;
+    for(i = 1;i <= MAX; i++){
+resultado = i *2 ;
+    printf("Indice %d : numero : %d\n",i,resultado);
+    }
+    return 0;
+}
