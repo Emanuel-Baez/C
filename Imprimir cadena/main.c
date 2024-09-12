7c5fb882d049fff24b5d3ac5f392873297ba3c1f
Emanuel <emanuelbenitezbaez@gmail.com>
2024-09-12 16:53:12 -0300

    Funcion imprimir cadena en mayusculas

---
+++ Imprimir cadena/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+//Prototipos
+//void imprimirCadena(char*);
+void convertirAmayuscula(char[]);
+int main()
+{
+    /*
+    Escribe una función que imprima una cadena de caracteres a mayuscula.
+    */
+
+    char cadena[25];
+    printf("Ingrese una palabra: \n");
+    fgets(cadena,sizeof(cadena),stdin);
+
+
+    convertirAmayuscula(cadena);
+    printf("La cadena en mayusculas es: %s\n", cadena);
+    return 0;
+}
+//Declaracion de funciones:
+
+
+
+void convertirAmayuscula(char cadena[]){
+int i = 0;
+while(cadena [i] != '\0'){
+    cadena[i]= toupper(cadena[i]);
+    i++;
+}
+
+}
